40e2f2c93f100007522b7f3d6f9e62db018c652a
Chirayu Chhabra <chirayuchhabra4@gmail.com>
2026-03-10 11:02:06 +0530

    add Vector class

---
+++ src/vector.ts
@@ -0,0 +1,57 @@
+class Vector {
+    readonly size: number;
+
+    constructor(public readonly coordinates: number[]) {
+        this.size = coordinates.length;
+    }
+
+    get norm(): number {
+        const sumSquare = this.coordinates.reduce(
+            (sum, coordinate) => sum + coordinate ** 2,
+            0,
+        );
+        return Math.sqrt(sumSquare);
+    }
+
+    private static _compatibilityCheck(vec1: Vector, vec2: Vector): void {
+        if (vec1.size != vec2.size) {
+            throw new Error(
+                `Invalid Vector sizes , Expected both vectors to be the same size but got vec1.size: ${vec1.size} | vec2.size: ${vec2.size}`,
+            );
+        }
+    }
+
+    static scale(vector: Vector, scaler: number): Vector {
+        const scaledCoordinates = vector.coordinates.map(
+            (coordinate) => coordinate * scaler,
+        );
+
+        return new Vector(scaledCoordinates);
+    }
+
+    static dot(vec1: Vector, vec2: Vector): number {
+        this._compatibilityCheck(vec1, vec2);
+        const sum = vec1.coordinates.reduce(
+            (partialSum, coord, index) =>
+                partialSum + coord * vec2.coordinates[index],
+            0,
+        );
+        return sum;
+    }
+
+    // this Generalized cross product was created using AI as I was not aware of the Generalized cross product and its nuances , though this reduces to 2d and 3d cross products that are well known to me  and are only ones that are going to be used at least for now.
+    static bivector(vec1: Vector, vec2: Vector): number[][] {
+        this._compatibilityCheck(vec1, vec2);
+        const n = vec1.size;
+        const biv = Array.from({ length: n }, () => new Array(n).fill(0));
+        for (let i = 0; i < n; i++) {
+            for (let j = i + 1; j < n; j++) {
+                biv[i][j] =
+                    vec1.coordinates[i] * vec2.coordinates[j] -
+                    vec1.coordinates[j] * vec2.coordinates[i];
+                biv[j][i] = -biv[i][j];
+            }
+        }
+        return biv;
+    }
+}
